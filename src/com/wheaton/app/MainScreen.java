3f5b318f81bf98c29f616a79a465758e11056ba5
drewhannay <devnull@localhost>
2010-12-18 15:52:31 -0600

    Restored basic Stalkernet functionally.  Use by clicking the top left button on the main screen.

--- src/com/wheaton/app/MainScreen.java
+++ src/com/wheaton/app/MainScreen.java
@@ -1,10 +1,10 @@
 package com.wheaton.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ImageView;
 
 public class MainScreen extends Activity implements OnClickListener {
 	
@@ -26,9 +26,14 @@ public class MainScreen extends Activity implements OnClickListener {
 		
 	}
 
-	public void onClick(View arg0) {
-		
-		
+	public void onClick(View v) {
+		Intent i;
+		switch(v.getId()){
+		case R.id.stalkernet:
+			i = new Intent(this,StalkernetHome.class);
+			startActivity(i);
+			break;
+		}
 		
 	}
 }
