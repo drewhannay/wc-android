893bfa723dc8bd531607d14770abad57994714eb
alisamaas <devnull@localhost>
2010-12-19 17:00:57 -0600

    Outline for parse() is finished.

--- src/com/wheaton/app/MenuParser.java
+++ src/com/wheaton/app/MenuParser.java
@@ -6,6 +6,13 @@ public class MenuParser {
 public static ArrayList<Day> days = new ArrayList<Day>();
 
 public static void parse(){
+if(days == null){
+	//TODO try to read from the file
 	
+	//TODO if no file, parse the html
+}
+else{
+	//TODO crop expired days by comparing to the current time/date
+}
 }
 }
