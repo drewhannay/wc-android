e2e08615a3dcd0de7df48de7953d25f2b71ed8ce
alisamaas <devnull@localhost>
2010-12-19 18:19:22 -0600

    Almost done with parse() method, just parsing the html left.

--- src/com/wheaton/app/MenuParser.java
+++ src/com/wheaton/app/MenuParser.java
@@ -1,15 +1,34 @@
 package com.wheaton.app;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Calendar;
 import java.util.Stack;
 import java.util.StringTokenizer;
+
+import android.content.Context;
+
 public class MenuParser {
 public static Stack<Day> days = new Stack<Day>();
 
-	public static void parse(){
+	public static void parse(Context con){
 		if(days.empty()){
-			//TODO try to read from the file
-	
+			if(con.fileList().length != 0){ 
+			
+        		FileInputStream f_in = null;
+				try {
+					f_in = con.openFileInput("days_cache");
+				
+        		// Read object using ObjectInputStream
+				ObjectInputStream obj_in;
+					obj_in = new ObjectInputStream (f_in);
+				days = (Stack<Day>) obj_in.readObject();
+				}catch(Exception e){
+					//this should never, ever happen since
+					//as of now days_cache is our ONLY file.
+				}
 			//TODO if no file, parse the html
 		}
 		else{
@@ -44,7 +63,17 @@ public static Stack<Day> days = new Stack<Day>();
 			//to avoid extra checking. But this will never happen more than
 			//once a day, at the most.
 			if(days.empty())
-				parse();
+				parse(con);
+			//Either way, at the end of the method, write the new object
+			//to data in case. TODO - put in the main menu class at some point?
+			//onPause? Seems like a good use for it...
+			try{FileOutputStream f_out = con.openFileOutput("days_cache", Context.MODE_PRIVATE);
+
+			// Write object with ObjectOutputStream
+			ObjectOutputStream obj_out = new ObjectOutputStream(f_out);
+			// Write object out to disk
+			obj_out.writeObject (days);}catch(Exception e){}
+		}
 		}
 	}
 }
