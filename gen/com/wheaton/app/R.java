7222a4842f69b0cf5cbe2fc7412833d9e744f333
drewhannay <devnull@localhost>
2010-12-29 22:41:45 -0600

    Added Link functionallity. Currently using sample links to Google and Facebook.

--- gen/com/wheaton/app/R.java
+++ gen/com/wheaton/app/R.java
@@ -48,13 +48,14 @@ public final class R {
     public static final class layout {
         public static final int about=0x7f030000;
         public static final int food_menu=0x7f030001;
-        public static final int links=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int map=0x7f030004;
-        public static final int menu_main=0x7f030005;
-        public static final int openfloor_main=0x7f030006;
-        public static final int results=0x7f030007;
-        public static final int stalkernet_main=0x7f030008;
+        public static final int link_item=0x7f030002;
+        public static final int links=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int map=0x7f030005;
+        public static final int menu_main=0x7f030006;
+        public static final int openfloor_main=0x7f030007;
+        public static final int results=0x7f030008;
+        public static final int stalkernet_main=0x7f030009;
     }
     public static final class menu {
         public static final int menu=0x7f060000;
--- src/com/wheaton/app/Links.java
+++ src/com/wheaton/app/Links.java
@@ -1,14 +1,62 @@
 package com.wheaton.app;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class Links extends Activity {
+/**
+ * Class to display links to various resources on the web.
+ * Keep static, final arrays of URLs and the text that should display
+ * in the ListActivity. Display the text and set the onClickListeners
+ * to launch an Intent to the browser when the list items are clicked.
+ * @author Drew Hannay
+ * December 29, 2010
+ *
+ */
+public class Links extends ListActivity {
 	
+	//To hold the text to be displayed for each list item.
+	//Each line is numbered as a sanity check when matching to the URL array.
+	private static final String[] TEXT = {
+		"Google",		//1
+		"Facebook"		//2
+		};
+	//To hold the URL to launch when an item is clicked.
+	private static final String[] URLS = {
+		"http://www.google.com",		//1
+		"http://www.facebook.com"		//2
+		};
+	
+	/**
+	 * Override the default onCreate method for a ListActivity.
+	 * Set the Activity's ListAdapter, enable the Text Filter,
+	 * and set the onItemClickListener.
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.links);
+		//Set the ListAdapter to read in the TEXT array, using the layout link_item.xml
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.link_item, TEXT));
+
+		//After getting the view, enable the text filter, so the user can type to narrow the search results.
+		ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
+	
+		//Set the OnItemClickListener.
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				//Launch an Intent to the browser using the URL in the same position as the TEXT that was clicked.
+				Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(URLS[position]));
+		    	startActivity(i);
+		    }
+		  });
 	}	
 
 }
