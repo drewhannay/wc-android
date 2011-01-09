5d6279f4f47e16f56d1687b78f1de98f9960f848
drewhannay <devnull@localhost>
2011-01-09 15:19:00 -0600

    Locked several Activities into portrait mode.

--- src/com/wheaton/app/OpenFloorHome.java
+++ src/com/wheaton/app/OpenFloorHome.java
@@ -17,7 +17,6 @@ public class OpenFloorHome extends Activity implements OnClickListener{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.openfloor_main);
 		
