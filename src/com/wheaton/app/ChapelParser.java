a7e41fc3d8730fdf861420ddaf753e71f57a28af
drewhannay <devnull@localhost>
2011-01-09 14:40:34 -0600

    Forgot to add files. Also added Maps API key.

---
+++ src/com/wheaton/app/ChapelParser.java
@@ -0,0 +1,25 @@
+package com.wheaton.app;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class ChapelParser {
+	
+	public static void parse(Context con){
+		
+	}
+
+	public static void crop(Context con, boolean reparse){
+		
+	}
+	
+	public static ArrayList<View> toArrayList(LayoutInflater l){
+		ArrayList<View> toReturn = new ArrayList<View>();
+		//WebView v;
+		
+		return toReturn;
+	}
+}
