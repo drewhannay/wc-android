008c3c3c3609305c0608538065981087ed0ea54d
alisamaas <devnull@localhost>
2011-07-20 21:40:17 -0600

    Fixed up most of the links for the new website - two, both originally in the registrar's site, now redirect to a www2.wheaton.edu site, the archived version. We need to check these later and insure they still work if they update these links. These links are marked by TODOs. Otherwise the rest should work.

--- src/com/wheaton/app/Links.java
+++ src/com/wheaton/app/Links.java
@@ -44,21 +44,24 @@ public class Links extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		t.put("Academic Calendar","http://wheaton.edu/Calendars/academic.html");
+		t.put("Academic Calendar","http://wheaton.edu/Academics/Academic-Calendar");
 		t.put("Arena Theater","http://www.wheatonarena.com");
 		t.put("Athletics","http://athletics.wheaton.edu");
 		t.put("Bookstore","http://www.wheatonbooks.com");
-		t.put("Chapel Schedule","http://www.wheaton.edu/chaplain/Program/schedule.html");
-		t.put("Conservatory","http://www.wheaton.edu/Conservatory");
-		t.put("Events Calendar","http://wheaton.edu/Calendars/events.html");
+		t.put("Chapel Schedule","http://www.wheaton.edu/Student-Life/Spiritual-Life/Chapel/Schedule");
+		t.put("Conservatory","http://www.wheaton.edu/academics/departments/conservatory");
+		t.put("Events Calendar","http://wheaton.edu/events");
+		//TODO The gen-ed link works for now but redirects to a www2.wheaton.edu site...test later.
 		t.put("General Education Requirements","http://wheaton.edu/Registrar/catalog/ug_acad_policies.htm#General_Education_Requirements");
 		t.put("HoneyRock","http://www.honeyrockcamp.org");
-		t.put("Housing Calendar","http://www.wheaton.edu/reslife/dates.htm");
+		t.put("Housing Calendar","http://www.wheaton.edu/Student-Life/Living-at-Wheaton/Calendar");
 		t.put("Library","http://library.wheaton.edu");
-		t.put("Long Term Calendar","http://www.wheaton.edu/Registrar/schedules/future_calendar.pdf");
+		t.put("Long Term Calendar","http://www.wheaton.edu/Academics/Services/Registrar/~/media/Files/Academics/Academic%20Services/Office%20of%20the%20Registrar/Schedule/future_calendar.pdf");
 		t.put("Metra Schedule","http://www.metrarail.com");
 		t.put("My Wheaton Portal","http://my.wheaton.edu");
-		t.put("Registrar's Catalog","http://www.wheaton.edu/Registrar/catalog");
+		//TODO Redirects to a www2.wheaton.edu site also - but that's also what I get when following
+		//a link from the new Wheaton site. Check this later in case they update the link.
+		t.put("Registrar's Catalog","http://www2.wheaton.edu/Registrar/catalog/index.htm");
 		t.put("WETN","http://www.wheaton.edu/wetn");
 		
 		//Set the ListAdapter to read in the TEXT array, using the layout link_item.xml
